39d2cb2972c761bf8d999a3b3cc392f60a9aef17
Sana Garah <sanambg98@gmail.com>
2021-01-20 12:04:42 +0300

    gitignore updated

--- App.js
+++ App.js
@@ -5,13 +5,12 @@ import { LogBox } from 'react-native';
 LogBox.ignoreLogs([
   "Your project is accessing the following APIs from a deprecated global rather than a module import: Constants (expo-constants).",
 ]);
-let user = "trainee";
+let user = "trainer";
 export default class app extends React.Component {
   render() {
    
       if(user=="trainer")
         return (<TrainerNav />)
         else if (user=="trainee") return (<TraineeNav />)
-  
   }
 }
