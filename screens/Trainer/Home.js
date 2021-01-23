1b34b2b29d72243bdabad225b3db9d5b14577b14
Abeer <abeercs.2020@gmail.com>
2021-01-23 23:07:45 +0300

    salwa123

--- screens/Trainer/Home.js
+++ screens/Trainer/Home.js
@@ -6,7 +6,7 @@ export default class Home extends React.Component {
     return (
       <View>
         <Text>Home</Text>
-        <Text>Salwa</Text>
+        <Text>Salwa 123</Text>
       </View>
     );
   }
