6d05ca501a6fd12b583181cd2981981f06da4e97
hopeht909 <hopeht909@gmail.com>
2021-01-23 22:53:38 +0300

    aaa

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { LogBox } from 'react-native';
 LogBox.ignoreLogs([
   "Your project is accessing the following APIs from a deprecated global rather than a module import: Constants (expo-constants).",
 ]);
-let user = "trainer";
+let user = "trainee";
 export default class app extends React.Component {
   render() {
    
--- screens/Trainee/Home.js
+++ screens/Trainee/Home.js
@@ -5,7 +5,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <View>
-        <Text>Home</Text>
+        <Text>Hi Ameera 12345676 </Text>
       </View>
     );
   }
