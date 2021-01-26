add07f704e799755577ce48ed85d89d54ce37a64
Sana Garah <sanambg98@gmail.com>
2021-01-26 17:36:17 +0300

    Fixed import errors

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { LogBox } from 'react-native';
 LogBox.ignoreLogs([
   "Your project is accessing the following APIs from a deprecated global rather than a module import: Constants (expo-constants).",
 ]);
-let user = "trainee";
+let user = "trainer";
 export default class app extends React.Component {
   render() {
 
--- screens/Trainee/Match.js
+++ screens/Trainee/Match.js
@@ -1,3 +1,5 @@
+import React from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import Deck from "../../components/Cards";
 
