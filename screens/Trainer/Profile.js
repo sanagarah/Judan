198ee1cf4609ece71a49e33bf176e3ba20d7419b
Sana Garah <sanambg98@gmail.com>
2021-01-26 22:28:36 +0300

    Comment edited

--- screens/Trainer/Profile.js
+++ screens/Trainer/Profile.js
@@ -128,7 +128,7 @@ export default class PopUps extends React.Component {
     console.log(result);
 
     if (!result.cancelled) {
-      {/*Saving the uri of the selected photo */ }
+      {/*Saving the uri of the selected photo*/ }
       this.setState({ image: result.uri });
     }
   };
