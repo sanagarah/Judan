1cbbe393d0d70b142ab55d72f06e4d1a455a672e
Abeer <abeercs.2020@gmail.com>
2021-01-28 19:00:33 +0300

    second try adding the new view in home.js trainer

--- screens/Trainee/Home.js
+++ screens/Trainee/Home.js
@@ -6,7 +6,11 @@ export default class Home extends React.Component {
     return (
       <View>
 <<<<<<< HEAD
+<<<<<<< HEAD
         <Text>Hi Ameera 12345676 </Text>
+=======
+        <Button title="Match me" onPress={() => { this.props.navigation.navigate('Match') }}></Button>
+>>>>>>> d8b58ccab6243338c82566fbdb1bf9da86da1b97
 =======
         <Button title="Match me" onPress={() => { this.props.navigation.navigate('Match') }}></Button>
 >>>>>>> d8b58ccab6243338c82566fbdb1bf9da86da1b97
