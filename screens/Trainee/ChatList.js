482c3eeb469c6dc78479e54b3de7f122b9fc27e3
Sana Garah <sanambg98@gmail.com>
2021-02-20 15:08:20 +0300

    ChatList and Chat pages have been added

--- screens/Trainee/ChatList.js
+++ screens/Trainee/ChatList.js
@@ -1,14 +1,40 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React from 'react';
+import { StyleSheet, View, ScrollView, SafeAreaView } from 'react-native';
+import ChatBox from "../../components/ChatBox"
 
 export default class ChatList extends React.Component {
+  state = { text: "" }
   render() {
     return (
-      <View>
-        <Text>ChatList</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        {/*View for ppl's msg*/}
+        <ScrollView>
+          <ChatBox
+            nav={() => this.props.navigation.navigate('Chating')}
+            name="Sana"
+            lastMassage="hi"
+            time="15 m">
+          </ChatBox>
+          <ChatBox
+            nav={() => this.props.navigation.navigate('Chating')}
+            name="Salwa"
+            lastMassage="hi, whats up"
+            time="15 m">
+          </ChatBox>
+          <ChatBox
+            nav={() => this.props.navigation.navigate('Chating')}
+            name="Aseel"
+            lastMassage="hi, whats up"
+            time="15 m">
+          </ChatBox>
+        </ScrollView>
+      </SafeAreaView>
     );
   }
 }
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
+  },
+});
--- screens/Trainer/Chat.js
+++ screens/Trainer/Chat.js
@@ -1,14 +1,46 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React from 'react';
+import { StyleSheet, View, SafeAreaView } from 'react-native';
+import ChatDpnd from '../../components/ChatDpnd';
+import Header from "../../components/ChatHeader";
 
 export default class Chat extends React.Component {
+  state = { text: "" }
   render() {
     return (
-      <View>
-        <Text>Chat</Text>
-      </View>
+      <SafeAreaView style={styles.container1}>
+
+        {/*view for header box*/}
+        <View style={styles.container2}>
+          <Header name="Sara" subname="Rigesterd" nav={() => this.props.navigation.navigate('Chat')}>
+          </Header>
+        </View>
+
+        {/*view for masseage box*/}
+        <View style={styles.container3}>
+          <ChatDpnd />
+        </View>
+      </SafeAreaView>
     );
   }
 }
+const styles = StyleSheet.create({
+  container1: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container2: {
+    height: 100,
+    backgroundColor: '#79D2AE',
+    width: '100%',
+    flexDirection: 'column',
+  },
+  container3: {
+    flex: 5,
+    backgroundColor: '#E7E8EF',
+    width: '100%',
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+});
 
-const styles = StyleSheet.create({});
